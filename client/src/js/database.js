909aad1400cefb26fb918e9fc2b9154ccef018cc
Mayur Patel <pmayur0680@gmail.com>
2022-05-26 18:54:32 -0500

    fixed error in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,7 +4,7 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        // console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
@@ -17,8 +17,8 @@ export const putDb = async (content) => {
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');  
   store.put({ id: 1, value: content})    
-  const result = await request;
-  console.log('🚀 - data saved to the database', result); 
+  // const result = await request;
+  // console.log('🚀 - data saved to the database', result); 
 };
 
 export const getDb = async () => {
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  console.log('result.value', result.value);
+  // console.log('result.value', result.value);
   return result.value;
 };
 
